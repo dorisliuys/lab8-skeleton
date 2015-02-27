f5e699c0a6f2b8eda07c1e3b248c799bfc3d9441
vagrant <vagrant@precise32.(none)>
2015-02-27 07:02:46 +0000

    analytic lab

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,26 @@ var projects = require("../projects.json")
 
 exports.view = function(req, res){
   // Here you should implement your random landing page rendering functionality
-  res.render("index", projects);
+  var random_num = Math.random();
+  if(random_num > 0.5){
+    res.render("index", projects);
+  }else{
+    res.render("index_alternate", projects)
+  }
 };
+
+/*
+exports.vGrid = function (req, res){
+	projects['grid'] = true;
+	res.render('index', projects);
+};
+
+expots.vAlternate = function (req, res){
+	var alternate = projects;
+	for(proj in alternate['projects']) {
+		alternate['projects'][proj]['title'] = "New Title";
+	}
+
+	res.render('index, alternate');
+}
+*/
